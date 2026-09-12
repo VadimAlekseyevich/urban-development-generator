e6a43a742e86b7f6eb553dcf26e705872cfa9dc6
Vadim <shilin.vadim.alekseyevich@gmail.com>
2026-09-12 18:57:20 +0300

    style: fix S01-T09 import source

--- core/urban_generator/domain/errors.py
+++ core/urban_generator/domain/errors.py
@@ -1,7 +1,8 @@
+from collections.abc import Mapping
 from dataclasses import dataclass
 from enum import StrEnum
 from types import MappingProxyType
-from typing import ClassVar, Mapping
+from typing import ClassVar
 
 
 class ErrorCategory(StrEnum):
