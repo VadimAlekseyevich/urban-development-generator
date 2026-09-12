c721140a59a8df16a300cfc8b9eeac8db09b5886
Vadim <shilin.vadim.alekseyevich@gmail.com>
2026-09-12 10:56:45 +0300

    fix(ci): sort Alembic imports

--- database/migrations/env.py
+++ database/migrations/env.py
@@ -3,9 +3,9 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from backend.app import models  # noqa: F401
 from backend.app.core.config import settings
 from backend.app.db.base import Base
-from backend.app import models  # noqa: F401
 
 config = context.config
 config.set_main_option("sqlalchemy.url", settings.database_url)
