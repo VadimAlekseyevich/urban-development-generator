a77734f6b8b701f2626bf2d4da57322e128e11c1
Vadim <shilin.vadim.alekseyevich@gmail.com>
2026-09-13 18:04:25 +0300

    fix: install frontend runtime crash diagnostics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import './styles.css'
+import './diagnostics.css'
 import App from './App'
+import { RuntimeDiagnostics } from './RuntimeDiagnostics'
+import { installGlobalDiagnostics } from './diagnostics'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+installGlobalDiagnostics()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RuntimeDiagnostics>
+    <App />
+  </RuntimeDiagnostics>,
+)
