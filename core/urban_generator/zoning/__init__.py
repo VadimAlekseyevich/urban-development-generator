fdb1da4fa6d8bc18886811d4b82fe0fba4820819
Vadim <shilin.vadim.alekseyevich@gmail.com>
2026-09-13 21:06:34 +0300

    feat: add S05-T01 functional zoning config
    * feat: add S05-T01 zoning domain config
    
    * feat: export S05-T01 zoning contracts
    
    * test: cover S05-T01 zoning config contract
    
    * docs: document S05-T01 zoning config contract

---
+++ core/urban_generator/zoning/__init__.py
@@ -0,0 +1,17 @@
+from core.urban_generator.zoning.config import (
+    ZoneAdjacencyPolicy,
+    ZoneAdjacencyRule,
+    ZoneClass,
+    ZoneClassConfig,
+    ZoningConfig,
+    ZoningConfigError,
+)
+
+__all__ = [
+    "ZoneAdjacencyPolicy",
+    "ZoneAdjacencyRule",
+    "ZoneClass",
+    "ZoneClassConfig",
+    "ZoningConfig",
+    "ZoningConfigError",
+]
