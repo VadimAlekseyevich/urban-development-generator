4c16275d0b328f743181f098b8caf17a5783c99c
Vadim <shilin.vadim.alekseyevich@gmail.com>
2026-09-15 00:03:29 +0300

    feat: export spatial snapping contract

--- core/urban_generator/roads/__init__.py
+++ core/urban_generator/roads/__init__.py
@@ -5,11 +5,23 @@ from core.urban_generator.roads.networkx_backend import (
     NetworkXBackend,
     NetworkXBackendError,
 )
+from core.urban_generator.roads.spatial_snapping import (
+    DEFAULT_MAX_SNAP_TARGETS,
+    SpatialSnapIndex,
+    SpatialSnapMatch,
+    SpatialSnapTarget,
+    SpatialSnappingError,
+)
 
 __all__ = [
+    "DEFAULT_MAX_SNAP_TARGETS",
     "EDGE_LENGTH_ATTRIBUTE",
     "NODE_X_ATTRIBUTE",
     "NODE_Y_ATTRIBUTE",
     "NetworkXBackend",
     "NetworkXBackendError",
+    "SpatialSnapIndex",
+    "SpatialSnapMatch",
+    "SpatialSnapTarget",
+    "SpatialSnappingError",
 ]
