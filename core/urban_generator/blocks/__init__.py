a625145c99bbb1bd993b689c625d91d8348e4917
Vadim <shilin.vadim.alekseyevich@gmail.com>
2026-09-16 14:07:01 +0300

    feat: export S07-T01 block polygonization contract

---
+++ core/urban_generator/blocks/__init__.py
@@ -0,0 +1,19 @@
+from core.urban_generator.blocks.polygonize import (
+    DEFAULT_MAX_CANDIDATE_BLOCKS,
+    DEFAULT_MAX_POLYGONIZE_EDGES,
+    BlockPolygonizationDiagnostics,
+    BlockPolygonizationError,
+    BlockPolygonizationResult,
+    CandidateBlock,
+    RoadNetworkBlockPolygonizer,
+)
+
+__all__ = [
+    "DEFAULT_MAX_CANDIDATE_BLOCKS",
+    "DEFAULT_MAX_POLYGONIZE_EDGES",
+    "BlockPolygonizationDiagnostics",
+    "BlockPolygonizationError",
+    "BlockPolygonizationResult",
+    "CandidateBlock",
+    "RoadNetworkBlockPolygonizer",
+]
