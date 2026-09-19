547605cc6843513d180444013d184f9bbbc52c89
Vadim <shilin.vadim.alekseyevich@gmail.com>
2026-09-19 19:02:16 +0300

    feat: export S09-T02 residential capacity

--- core/urban_generator/demography/__init__.py
+++ core/urban_generator/demography/__init__.py
@@ -1,3 +1,11 @@
+from core.urban_generator.demography.capacity import (
+    DEFAULT_MAX_RESIDENTIAL_CAPACITY_SUBJECTS,
+    ResidentialBuildingCapacity,
+    ResidentialCapacityCalculator,
+    ResidentialCapacityError,
+    ResidentialCapacityResult,
+    ResidentialCapacitySubject,
+)
 from core.urban_generator.demography.config import (
     AgeGroupShare,
     DemographicScenario,
@@ -8,8 +16,14 @@ from core.urban_generator.demography.config import (
 
 __all__ = [
     "AgeGroupShare",
+    "DEFAULT_MAX_RESIDENTIAL_CAPACITY_SUBJECTS",
     "DemographicScenario",
     "DemographicScenarioError",
     "PopulationTarget",
     "PopulationTargetKind",
+    "ResidentialBuildingCapacity",
+    "ResidentialCapacityCalculator",
+    "ResidentialCapacityError",
+    "ResidentialCapacityResult",
+    "ResidentialCapacitySubject",
 ]
