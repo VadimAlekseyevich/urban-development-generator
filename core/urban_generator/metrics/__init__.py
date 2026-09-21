d2b836481c3574de15fa6a670325dc2db55fd5ba
Vadim <shilin.vadim.alekseyevich@gmail.com>
2026-09-21 08:41:21 +0300

    S11-T05 add metrics adapter package

---
+++ core/urban_generator/metrics/__init__.py
@@ -0,0 +1,19 @@
+from core.urban_generator.metrics.land_building import (
+    LAND_BUILDING_RAW_METRIC_IDS,
+    BuildingArchetypeShare,
+    LandBuildingMetricAdapter,
+    LandBuildingMetricAdapterError,
+    LandBuildingMetricDiagnostics,
+    LandBuildingRawMetricValue,
+    LandBuildingRawMetricsResult,
+)
+
+__all__ = [
+    "LAND_BUILDING_RAW_METRIC_IDS",
+    "BuildingArchetypeShare",
+    "LandBuildingMetricAdapter",
+    "LandBuildingMetricAdapterError",
+    "LandBuildingMetricDiagnostics",
+    "LandBuildingRawMetricValue",
+    "LandBuildingRawMetricsResult",
+]
