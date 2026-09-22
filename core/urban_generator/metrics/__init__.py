7ef4cc09bc0d1e58303240854f657033fdd0ccdc
Vadim <shilin.vadim.alekseyevich@gmail.com>
2026-09-22 10:44:09 +0300

    S11-T07 export demography metric adapter

--- core/urban_generator/metrics/__init__.py
+++ core/urban_generator/metrics/__init__.py
@@ -1,3 +1,11 @@
+from core.urban_generator.metrics.demography import (
+    DEMOGRAPHY_RAW_METRIC_IDS,
+    DemographyMetricAdapter,
+    DemographyMetricAdapterDiagnostics,
+    DemographyMetricAdapterError,
+    DemographyRawMetricsResult,
+    DemographyRawMetricValue,
+)
 from core.urban_generator.metrics.land_building import (
     LAND_BUILDING_RAW_METRIC_IDS,
     BuildingArchetypeShare,
@@ -17,6 +25,12 @@ from core.urban_generator.metrics.roads import (
 )
 
 __all__ = [
+    "DEMOGRAPHY_RAW_METRIC_IDS",
+    "DemographyMetricAdapter",
+    "DemographyMetricAdapterDiagnostics",
+    "DemographyMetricAdapterError",
+    "DemographyRawMetricValue",
+    "DemographyRawMetricsResult",
     "LAND_BUILDING_RAW_METRIC_IDS",
     "BuildingArchetypeShare",
     "LandBuildingMetricAdapter",
